bc5a799fa40c746c880fe87b2003351e7b7b3f6b
Simon Naumov <32277890+nothingelsematters@users.noreply.github.com>
2018-06-06 15:27:33 +0300

    :heavy_plus_sign: cpp big integer update

--- Cpp/BigInteger10.cpp
+++ Cpp/BigInteger10.cpp
@@ -8,9 +8,9 @@ const int MAX = 1000000000;
 
 class BigInteger {
 private:
-        vector<uint32_t> digits;
-        bool sign;
-friend ostream& operator <<(ostream& wave, const BigInteger& b) {
+    vector<uint32_t> digits;
+    bool sign;
+    friend ostream& operator <<(ostream& wave, const BigInteger& b) {
         if (!b.sign) wave << '-';
         wave << b.digits.back();
         for (int i = b.digits.size() - 2; i >= 0; --i) {
@@ -19,29 +19,29 @@ friend ostream& operator <<(ostream& wave, const BigInteger& b) {
                 wave << b.digits[i];
         }
         return wave;
-}
-friend istream& operator >>(istream& wave, BigInteger& b) {
+    }
+    friend istream& operator >>(istream& wave, BigInteger& b) {
         string s;
         wave >> s;
         b.sign = (s.substr(0, 1) != "-");
         if (!b.sign) s = s.substr(1, s.length() - 1);
         for (int i = BASE; i <= s.length(); i += BASE) {
-                b.digits.push_back(stoi(s.substr(s.length() - i, BASE)));
+            b.digits.push_back(stoi(s.substr(s.length() - i, BASE)));
         }
         if (s.length() % BASE != 0) {
-                b.digits.push_back(stoi(s.substr(0, s.length() % BASE)));
+            b.digits.push_back(stoi(s.substr(0, s.length() % BASE)));
         }
         return wave;
-}
+    }
 public:
-friend BigInteger operator -(BigInteger b){
+    friend BigInteger operator -(BigInteger b){
         b.sign = !b.sign;
         return b;
-}
-bool operator >(BigInteger const& b) {
+    }
+    bool operator >(BigInteger const& b) {
         return ((digits.size() > b.digits.size()) || ((digits.size() == b.digits.size()) && (digits.back() > b.digits.back())));
-}
-BigInteger operator -(BigInteger b) {
+    }
+    BigInteger operator -(BigInteger b) {
         if (sign != b.sign) return ((!sign) ? (-(-*this + b)) : (*this + -b));
         if (!sign) return (-b - -*this);
         if (*this > b) return (-(b - *this));
@@ -52,12 +52,12 @@ BigInteger operator -(BigInteger b) {
                 b.digits[i] = c + (carry ? MAX : 0);
         }
         return -b;
-}
-BigInteger& operator -=(BigInteger const& b) {
+    }
+    BigInteger& operator -=(BigInteger const& b) {
         *this = *this - b;
         return *this;
-}
-BigInteger operator +(BigInteger b) {
+    }
+    BigInteger operator +(BigInteger b) {
         if (sign != b.sign) return ((!sign) ? (b - -*this) : (*this - -b));
         int carry = 0;
         for (int i = 0; i < max(b.digits.size(), digits.size()) || carry; ++i) {
@@ -67,18 +67,18 @@ BigInteger operator +(BigInteger b) {
                 if (carry) b.digits[i] -= MAX;
         }
         return b;
-}
-BigInteger& operator +=(BigInteger const& b) {
+    }
+    BigInteger& operator +=(BigInteger const& b) {
         *this = *this + b;
         return *this;
-}
-BigInteger () {
+    }
+    BigInteger () {
         sign = true;
-}
-BigInteger (int32_t b) {
+    }
+    BigInteger (int32_t b) {
         digits.push_back(abs(b));
         sign = (b > 0);
-}
+    }
 };
 
 int main() {
