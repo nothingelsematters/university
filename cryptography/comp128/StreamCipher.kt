d11d8dfc84b48d1a059e3d7d47af21e559210d48
nothingelsematters <daretoodefy@gmail.com>
2019-11-19 01:21:32 +0300

    :shield: crypto comp128: debugging pending still

--- cryptography/comp128/StreamCipher.kt
+++ cryptography/comp128/StreamCipher.kt
@@ -98,3 +98,37 @@ fun streamEncrypt(key: ByteArray, message: ByteArray): ByteArray {
         }
         .toByteArray()
 }
+
+fun main() {
+    val key = listOf(0x12, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF).map(Int::toByte).toByteArray()
+    val frame = 0x134
+    val expected = listOf(0x53, 0x4E, 0xAA, 0x58, 0x2F, 0xE8, 0x15, 0x1A, 0xB6, 0xE1, 0x85, 0x5A, 0x72, 0x8C, 0x00,
+        0x24, 0xFD, 0x35, 0xA3, 0x5D, 0x5F, 0xB6, 0x52, 0x6D, 0x32, 0xF9, 0x06, 0xDF, 0x1A, 0xC0).map(Int::toByte)
+
+    val cluster = ShiftCluster(
+        listOf(
+            ShiftRegister(19, listOf(13, 16, 17, 18), 8),
+            ShiftRegister(22, listOf(20, 21), 10),
+            ShiftRegister(23, listOf(7, 20, 21, 22), 10)
+        )
+    )
+
+    cluster.launch(key, frame)
+    val result = cluster
+        .getBlock()
+        .toList()
+        .chunked(Byte.SIZE_BITS) {
+            it.foldIndexed(0) { index, result, bit -> if (bit) result or (1 shl index) else result }.toByte()
+        }
+
+    println("expected: ${expected.map { it.toBits() }.joinToString(separator = "")}")
+    println("result:   ${result.map { it.toBits() }.joinToString(separator = "")}")
+}
+
+fun Byte.toBits(): String {
+    var res = ""
+    for (i in 0..7) {
+        res += if (ushr(7 - i).and(1) == 1.toByte()) "1" else "0"
+    }
+    return res
+}
